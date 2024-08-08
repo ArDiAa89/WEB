020d0398062f789e6d3754657aca10e81b71069c
ArDiAa89 <gitardihub.dev@gmail.com>
2024-08-08 22:54:30 +0200

    Add files via upload

---
+++ HT/script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    const message = document.getElementById('U')
+    const message2 = document.getElementById('E');
+   const message3=document.getElementById('B');
+    message.style.opacity = 1; // Fade in the message
+    message.style.fontSize="4rem";
+    message2.style.opacity=1;
+    message2.style.fontSize="3rem";
+   message3.style.opacity=1;
+   message3.style.fontSize="3rem";
+  
+};
+ 
